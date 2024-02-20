8dfa087118ddac1a97985871d33ddeffd6b347d8
lauren-oseni-2 <lauren.oseni@farsightconsulting.co.uk>
2024-02-20 18:13:32 +0000

    Branching

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,23 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+router.post('/country-answer', function(request, response) {
+
+    var country = request.session.data['country']
+    if (country == "England"){
+        response.redirect("/before-you-continue")
+    } else {
+        response.redirect("/not-eligible")
+    }
+})
+
+router.post('/issues-before-answer', function(request, response) {
+
+    var issued = request.session.data['issued']
+    if (issued == "Yes"){
+        response.redirect("/non-eligible")
+    } else {
+        response.redirect("/current-address")
+    }
+})
