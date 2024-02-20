14f1be2b9aa717c1ce2ca6bda665e0912acad75d
lauren-oseni-2 <lauren.oseni@farsightconsulting.co.uk>
2024-02-20 19:38:41 +0000

    More branching

--- app/routes.js
+++ app/routes.js
@@ -12,18 +12,38 @@ router.post('/country-answer', function(request, response) {
 
     var country = request.session.data['country']
     if (country == "England"){
-        response.redirect("/before-you-continue")
+        response.redirect("/index")
     } else {
         response.redirect("/not-eligible")
     }
 })
 
-router.post('/issues-before-answer', function(request, response) {
+router.post('/issued-answer', function(request, response) {
 
     var issued = request.session.data['issued']
     if (issued == "Yes"){
-        response.redirect("/non-eligible")
+        response.redirect("/not-eligible")
     } else {
         response.redirect("/current-address")
+    }
+})
+
+router.post('/address-answer', function(request, response) {
+
+    var address = request.session.data['address']
+    if (address == "England"){
+        response.redirect("/before-you-continue")
+    } else {
+        response.redirect("/cannot-apply")
+    }
+})
+
+router.post('/intend-answer', function(request, response) {
+
+    var intend = request.session.data['intend']
+    if (intend == "Yes"){
+        response.redirect("/before-you-continue")
+    } else {
+        response.redirect("/cannot-apply")
     }
 })
